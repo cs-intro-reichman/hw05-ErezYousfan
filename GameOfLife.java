f50d9ea2cb8d7fefea1ddc273eff6848e4d68002
Erez Yousfan <erez.yousfan@post.runi.ac.il>
2024-01-15 19:19:50 +0200

    try four

--- GameOfLife.java
+++ GameOfLife.java
@@ -146,7 +146,9 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
-				System.out.print(arr[i][j] + "  ");
+				System.out.print(arr[i][j]);
+				if (j < (arr[0].length - 1))
+					System.out.print("  ");
 			}
 			System.out.println();
 		}
